80e41f1048cffb762269892c2283565084ab8a09
Sewon <tpdnjs7878@naver.com>
2025-09-10 16:37:03 +0900

    feat : 배포 테스트

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -9,7 +9,7 @@ import Login from "./views/Login.vue";
 <template>
   <div class="layout">
     <div class="content">
-      <!-- 테스트 1 -->
+      <!-- 테스트 2 -->
       <Login />
     </div>
   </div>
