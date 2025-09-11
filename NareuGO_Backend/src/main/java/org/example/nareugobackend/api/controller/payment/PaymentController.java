cc093602dbb012c081733324a7bbead02e55a1d6
Sewon <tpdnjs7878@naver.com>
2025-09-11 14:12:25 +0900

    백엔드 결제 기능을 위한 디렉토리 설정

---
+++ NareuGO_Backend/src/main/java/org/example/nareugobackend/api/controller/payment/PaymentController.java
@@ -0,0 +1,5 @@
+package main.java.org.example.nareugobackend.api.controller.payment;
+
+public class PaymentController {
+
+}
---
+++ NareuGO_Backend/src/main/java/org/example/nareugobackend/api/service/payment/PaymentServiceImpl.java
@@ -0,0 +1,5 @@
+package main.java.org.example.nareugobackend.api.service.payment;
+
+public class PaymentServiceImpl {
+
+}
