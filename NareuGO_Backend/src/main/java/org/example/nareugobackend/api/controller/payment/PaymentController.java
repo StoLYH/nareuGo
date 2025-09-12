8e96080d297389999e05a533a25775b0b00832fa
Sewon <tpdnjs7878@naver.com>
2025-09-12 15:56:15 +0900

    feat: 결제 승인 컨트롤러 구현

--- NareuGO_Backend/src/main/java/org/example/nareugobackend/api/controller/payment/PaymentController.java
+++ NareuGO_Backend/src/main/java/org/example/nareugobackend/api/controller/payment/PaymentController.java
@@ -1,5 +1,25 @@
 package org.example.nareugobackend.api.controller.payment;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.nareugobackend.api.controller.payment.request.PaymentConfirmRequestDto;
+import org.example.nareugobackend.api.service.payment.PaymentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payments")
+@RequiredArgsConstructor
 public class PaymentController {
 
+    private final PaymentService paymentService;
+
+    @PostMapping("/confirm")
+    public ResponseEntity<Void> confirmPayment(@Valid @RequestBody PaymentConfirmRequestDto requestDto) {
+        paymentService.confirmPayment(requestDto);
+        return ResponseEntity.noContent().build();
+    }
 }
