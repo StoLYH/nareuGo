07044c42bfef6ee54afbfc65c6c937ac5202e975
YongHoon <yy3082@naver.com>
2025-09-19 11:09:48 +0900

    fix: websocket경로수정

--- NareuGO_Backend/src/main/java/org/example/nareugobackend/config/WebSocketConfig.java
+++ NareuGO_Backend/src/main/java/org/example/nareugobackend/config/WebSocketConfig.java
@@ -11,9 +11,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Value("${server.env}")
-    private String env;
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // 서버 → 클라이언트 : 구독용 prefix 정의
@@ -25,10 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // 환경에 따라 엔드포인트 설정
-        String endpoint = "local".equals(env) ? "/ws" : "/api/ws";
-        
-        registry.addEndpoint(endpoint)
+        registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
