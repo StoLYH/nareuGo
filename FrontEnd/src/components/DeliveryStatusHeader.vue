ff20f455703793202dd35f9e45c3c55bc64cbf43
Sewon <tpdnjs7878@naver.com>
2025-09-24 10:36:52 +0900

    fix: 택배 페이지 헤더에 설정아이콘  대신 알림 아이콘 변경

--- FrontEnd/src/components/DeliveryStatusHeader.vue
+++ FrontEnd/src/components/DeliveryStatusHeader.vue
@@ -14,12 +14,17 @@
             <path d="M12 3a9 9 0 0 0-9 9v5a3 3 0 0 0 3 3h1a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2H5a7 7 0 0 1 14 0h-2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h1a3 3 0 0 0 3-3v-5a9 9 0 0 0-9-9Z" fill="currentColor"/>
           </svg>
         </button>
-        <button class="icon-btn" @click="openSettings" aria-label="설정">
-          <!-- Gear Icon -->
+        <!-- <button class="icon-btn" @click="openSettings" aria-label="설정">
           <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 15.5A3.5 3.5 0 1 1 12 8.5a3.5 3.5 0 0 1 0 7Zm7.43-2.52c.03-.32.07-.64.07-.98s-.04-.66-.07-.98l2.11-1.65a.5.5 0 0 0 .11-.65l-2-3.46a.5.5 0 0 0-.61-.22l-2.49 1a7.04 7.04 0 0 0-1.7-.98l-.38-2.65A.5.5 0 0 0 13 2h-2a.5.5 0 0 0-.5.42l-.38 2.65c-.6.23-1.17.55-1.7.98l-2.49-1a.5.5 0 0 0-.61.22l-2 3.46a.5.5 0 0 0 .11.65L5.94 11c-.03.32-.07.64-.07.98s.04.66.07.98l-2.11 1.65a.5.5 0 0 0-.11.65l2 3.46c.13.23.4.32.61.22l2.49-1c.53.43 1.1.75 1.7.98l.38 2.65c.03.24.25.42.5.42h2c.25 0 .47-.18.5-.42l.38-2.65c.6-.23 1.17-.55 1.7-.98l2.49 1c.21.1.48.01.61-.22l2-3.46a.5.5 0 0 0-.11-.65L19.43 12.98Z" fill="currentColor"/>
           </svg>
-        </button>
+        </button> -->
+
+        <button class="icon-btn" @click="goNotifications" title="알림">
+        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" focusable="false" aria-hidden="true">
+          <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z" fill="currentColor"/>
+        </svg>
+      </button> 
       </div>
     </div>
     
@@ -54,6 +59,8 @@ const router = useRouter()
 const goBack = () => router.back()
 const openSupport = () => console.log('고객센터 열기')
 const openSettings = () => console.log('설정 열기')
+const goNotifications = () => router.push({ name: 'Notifications' });
+
 </script>
 
 
